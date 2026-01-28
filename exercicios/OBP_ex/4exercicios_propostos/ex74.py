47d0ca88ed891589dbe30042bc0941e2940ef5d5
leitbom <chenriquecandido@gmail.com>
2026-01-28 00:22:36 +0000

    ex74.py biggest exercice list ever!

--- exercicios/OBP_ex/4exercicios_propostos/ex74.py
+++ exercicios/OBP_ex/4exercicios_propostos/ex74.py
@@ -1,3 +1,12 @@
 ## Do a program that calculates and prints the terms of the series X**N/N! where X and N should be read from the input.
 ## Faça um programa que calcule e imprima os termos da série X**N/N! onde X e N devem ser lidos da entrada.
 
+try:
+    x = float(input("Enter a number: "))
+    n = int(input("Enter an integer number: "))
+    from math import factorial as fa
+    result  = x**n/fa(n)
+except:
+    print("You've done something wrong.")
+else:
+    print(f'{result:.2f}')
